483d89f73c787e6fdd44bee041809088249c73c9
snerks <martin.github@outlook.com>
2016-09-20 23:23:35 +0100

    Feature1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Feature1Component } from './feature1/feature1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Feature1Component
   ],
   imports: [
     BrowserModule,
